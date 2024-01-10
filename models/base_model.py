10952fb5b4a0477f5bf6163857c7a9dd62971d7d
Philippa <p.giibwa@alustudent.com>
2024-01-10 10:40:34 +0000

    changes

--- models/base_model.py
+++ models/base_model.py
@@ -23,8 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage.new(self)
-    
+            models.storage.new(self)    
     def __str__(self):
         """returns string representation of an instance"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
