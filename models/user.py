9a844ca2c948864d908abff68dc1337cd0cd30d6
Euwamahoro <e.uwamahoro@alustudent.com>
2024-01-10 12:30:48 +0200

    This is Enock

--- models/user.py
+++ models/user.py
@@ -1,20 +1,22 @@
+#!/usr/bin/python3
+"""
+    Define 'User' class
+"""
 from models.base_model import BaseModel
+
 
 class User(BaseModel):
     """
-    User class that inherits from BaseModel.
-    Public class attributes:
-        email: string - empty string
-        password: string - empty string
-        first_name: string - empty string
-        last_name: string - empty string
+        Class to manage user objects
+        Represent user with an email, password, first name and last name
+        Attributes:
+            email (str): user's email
+            password (str): user's password
+            first_name (str): user's first name
+            last_name (str): user's last name
     """
-    def __init__(self, *args, **kwargs):
-        """
-        Initialize User instance.
-        """
-        super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
